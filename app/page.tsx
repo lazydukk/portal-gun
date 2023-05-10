bd0fcbedd3e741a1684e9966dcab2cf9691ad341
bdas <114676073+lazydukk@users.noreply.github.com>
2023-05-10 10:57:56 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-      <main className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-800 to-purple-900">
-        <h1 className="text-4xl text-white text-center mb-4">lazydukk</h1>
+      <main className="flex flex-col items-center justify-center min-h-screen bg-slate-900">
+        <h1 className="text-4xl text-blue-500 text-center mb-4">lazydukk</h1>
         <p className="text-xl text-white text-center">portal gun for lazdukk's portfolio in the planet C137</p>
     </main>
   )
