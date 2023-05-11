8a774ea18582e0a587720a0c36d941a06beaf5c7
bdas <114676073+lazydukk@users.noreply.github.com>
2023-05-12 03:28:22 +0530

    gihub link redirected to profile /lazydukk

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <h1 className="text-9xl text-indigo-600 text-center mb-4">lazydukk</h1>
         <p className="text-xl text-white text-center">portal gun for lazydukk&apos;s portfolio in the planet C137</p>
         <span className="font-roboto text-sm text-gray-400">on hold as of exams </span>
-        <a href="https://github.com/lazydukk/portal-gun" className="font-roboto mr-4 hover:underline md:mr-6 ">Github</a>
+        <a href="https://github.com/lazydukk/" className="font-roboto mr-4 hover:underline md:mr-6 ">Github</a>
       </main>
       </>
   )
