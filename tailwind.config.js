6c48c63d8d574a546e9a76aeae3c2795de8ab710
bdas <114676073+lazydukk@users.noreply.github.com>
2023-05-12 21:16:10 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,6 @@ module.exports = {
       fontFamily: {
         pop: ['var(--font-pop)'],
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
           },
   },
   plugins: [],
