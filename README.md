a156ecd703a785302e3ae3a0b2bb526d02e82f57
lazydukk <bytex07@proton.me>
2023-08-28 04:16:47 +0530

    nuke && tailwind add

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
 # lazydukk/portal-gun
-
-* made w/ astro, tailwind, react components, mdx
-* hosted on vercel
-* inspired by theo and astro's awsomeness
-
-### Chores:
-1. add social links: ✅
-2. add a navbar
-3. make mobile responsive 
-4. add image optimization
-5. confid mdx / itegrate blogging
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('@catppuccin/tailwindcss')({
-			// prefix to use, e.g. `text-pink` becomes `text-ctp-pink`.
-			// default is `false`, which means no prefix
-			prefix: 'cpcn',
-			// which flavour of colours to use by default, in the `:root`
-			defaultFlavour: 'mocha'
-		  }),
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@catppuccin/tailwindcss")({
+      // prefix to use, e.g. `text-pink` becomes `text-ctp-pink`.
+      // default is `false`, which means no prefix
+      prefix: "cpcn",
+      // which flavour of colours to use by default, in the `:root`
+      defaultFlavour: "mocha",
+    }),
+  ],
+};
