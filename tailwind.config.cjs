6c11a10aa7819e03ca5d2426dd4c836391a8c0e3
lazydukk <bytex07@proton.me>
2023-08-28 04:36:17 +0530

    index.astro updated w/ taiwlind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
     require("@catppuccin/tailwindcss")({
       // prefix to use, e.g. `text-pink` becomes `text-ctp-pink`.
       // default is `false`, which means no prefix
-      prefix: "cpcn",
+      prefix: "cp",
       // which flavour of colours to use by default, in the `:root`
       defaultFlavour: "mocha",
     }),
